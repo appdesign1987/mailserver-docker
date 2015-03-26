3313e1db482f4c1c9f7e12fc09e1fbcb9e655324
Jeroen van Drongelen <jeroenvandrongelen@funda.nl>
2015-03-26 06:43:56 +0100

    Added card and cal adresses to work with own server.

--- conf/zpush/backend_caldav.php
+++ conf/zpush/backend_caldav.php
@@ -5,9 +5,9 @@
 * Descr     :   CalDAV backend configuration file
 ************************************************/
 
-define('CALDAV_SERVER', 'https://localhost');
+define('CALDAV_SERVER', 'https://cal.jeroenvd.nl');
 define('CALDAV_PORT', '443');
-define('CALDAV_PATH', '/caldav/calendars/%u/');
+define('CALDAV_PATH', '/cal.php/calendars/%u/');
 define('CALDAV_PERSONAL', '');
 define('CALDAV_SUPPORTS_SYNC', true);
 
